1a43a5ac2f6edf9c1db9792b14f74fab47d85ee6
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-02-23 08:55:26 +0100

    still taking jonas course

---
+++ course folder/complete-javascript-course-master/17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,86 @@
+/*
+// Importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart';
+// addToCart('bread', 5);
+// console.log(price, totalQuantity);
+
+// import * as ShoppingCart from './shoppingCart.js';
+
+console.log('importing module');
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);
+
+// console.log('Start fetching');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// const data = await res.json();
+// console.log(data);
+// console.log('Something');
+
+const getLastPost = async function () {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+const lastPost = await getLastPost();
+console.log(lastPost);
+
+
+import shoppingCart from './shoppingCart.js';
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function () {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
+*/
+
+import cloneDeep from '../../node_modules/lodash-es/cloneDeep.js';
+console.log(cloneDeep);
+
+// import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+console.log(stateClone);
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
