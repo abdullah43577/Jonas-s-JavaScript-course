62257174e6fc5d62dfc6be47e63e3272fb2f2376
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-03-10 21:44:38 +0100

    continued taking jonas course

--- course folder/complete-javascript-course-master/18-forkify/starter/src/js/controller.js
+++ course folder/complete-javascript-course-master/18-forkify/starter/src/js/controller.js
@@ -1,3 +1,8 @@
+import * as model from './module';
+import recipeView from './view/recipeView';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 const recipeContainer = document.querySelector('.recipe');
 
 const timeout = function (s) {
@@ -11,3 +16,26 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 1) Loading Recipe
+    await model.loadRecipe(id);
+
+    // 2) rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+controlRecipes();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
+
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
